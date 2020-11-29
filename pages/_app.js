f8f9bbe813852b44c2e145bb3b33acb0a40290cd
Shmulik Flint <shmulikf@wix.com>
2020-11-29 02:27:06 +0200

    Add explanation in _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import './style.scss'
 import 'draft-js/dist/Draft.css'
 
+// this dummy component is needed to load global CSS files
 export default function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
