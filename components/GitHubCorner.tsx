db3781a993687f6ecae43e8d57cb0833f5c6f6ec
Shmulik Flint <shmulikf@wix.com>
2020-12-19 22:43:39 +0200

    Fuix eslint warning

--- components/GitHubCorner.tsx
+++ components/GitHubCorner.tsx
@@ -1,5 +1,5 @@
-import React from 'react'
+import React, { ReactElement } from 'react'
 import GithubCorner from 'react-github-corner'
 
-export const GitHubCorner = () =>
+export const GitHubCorner = (): ReactElement =>
   <GithubCorner href="https://github.com/splintor/yeruham-phone-book-web-site" size={40} target="YerhuamPhonebookGitHubProject"/>
