2c3dc40192e352cb66bc459e87a944d6edbbf926
Shmulik Flint <shmulikf@wix.com>
2021-01-24 18:56:50 +0200

    opensearch: Make suggestions url use rewrite to hopefully increase its performance

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   target: 'server',
+  async rewrites() {
+    return [
+      {
+        source: '/search-suggestions/:searchTerm',
+        destination: `${process.env.WIX_PAGE_URL}/_functions/search/:searchTerm?suggestions=1`,
+      },
+    ]
+  },
 }
--- pages/api/search-suggestions/[searchTerm].ts
+++
@@ -1,8 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next'
-import { getRequestLogData, sendResponse } from '../../../utils/api'
-import { getSearchSuggestions } from '../../../utils/data-layer'
-
-export default async function searchSuggestions(request: NextApiRequest, response: NextApiResponse): Promise<void> {
-  const { query: { searchTerm } } = request
-  return sendResponse(response, await getSearchSuggestions(request, searchTerm as string), getRequestLogData(request), 'application/x-suggestions+json')
-}
