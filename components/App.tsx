6f654039ad76253a49a4027c305abc6fb405a1f6
Shmulik Flint <shmulikf@wix.com>
2021-02-25 23:28:51 +0200

    Add general stuff

--- components/App.tsx
+++ components/App.tsx
@@ -369,16 +369,23 @@ export default function App(appProps: AppProps): ReactElement {
   useEffect(() => setAuthData(parseAuthCookies()), [])
   const isPageAllowed = status !== 401 && (authData?.auth || !newPage)
 
-  return <div className="app">
+  // suppress JSUnresolvedLibraryURL
+  // noinspection JSUnresolvedLibraryURL
+  return <html dir="rtl" lang="he"><div className="app">
     {showPreview && <Head>
       <title>{pageTitle}</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <meta property="og:title" content={pageTitle} key="pageTitle"/>
       {isPublicPage && <meta property="og:description" content={page.html.replace(/<[^>]+>|&nbsp;/g, ' ')} key="pageHtml"/>}
       <meta property="og:url" content={url} key="url"/>
       <meta property="og:image" content={`${origin}/logo.png`} key="image"/>
       <link rel="icon" href="/favicon.ico"/>
       <link rel="search" type="application/opensearchdescription+xml" title="חיפוש בספר הטלפונים של ירוחם" href="opensearch.xml" />
+      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.rtl.min.css"
+            integrity="sha384-4dNpRvNX0c/TdYEbYup8qbjvjaMrgUPh+g4I03CnNtANuv+VAvPL6LqdwzZKV38G" crossOrigin="anonymous"/>
     </Head>}
     {authData ? isPageAllowed ? <AppComponent authData={authData} {...appProps} /> : <LoginPage/> : ''}
-  </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossOrigin="anonymous"/>
+  </div></html>
 }
