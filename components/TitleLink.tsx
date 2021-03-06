5c07c046860e1f44471268efe0b0b59450d7a8c9
Shmulik Flint <shmulikf@wix.com>
2021-03-07 00:01:06 +0200

    Remove unused style prop from TitleLink

--- components/TitleLink.tsx
+++ components/TitleLink.tsx
@@ -1,5 +1,5 @@
 import React, { MouseEventHandler, ReactElement } from 'react'
 import { pageUrl } from '../utils/url'
 
-export const TitleLink = ({ title, href, onClick, className, style }: { title: string, href?: string, onClick?: MouseEventHandler, className?: string, style?: object}): ReactElement =>
+export const TitleLink = ({ title, href, onClick, className }: { title: string, href?: string, onClick?: MouseEventHandler, className?: string, style?: object}): ReactElement =>
   <a className={className} href={href || pageUrl(title)} onClick={onClick}>{title}</a>
