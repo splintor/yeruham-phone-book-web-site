50a90fddbf0fa647bc4f2336ee890e9306e053fb
Shmulik Flint <shmulikf@wix.com>
2021-05-08 23:51:11 +0300

    Add key to avoid react warning

--- components/AppComponent.tsx
+++ components/AppComponent.tsx
@@ -236,7 +236,7 @@ export function AppComponent(appProps: AppProps & { authData: AuthData }): React
                       }}/>
                     </h5>
                     <div>
-                      {page.tags?.map(tag => <TagLink tag={tag} pushState={pushState} kind="small"/>)}
+                      {page.tags?.map(tag => <TagLink key={tag} tag={tag} pushState={pushState} kind="small"/>)}
                     </div>
                     <input type="checkbox" id={page.title}/>
                     <PageHtmlRenderer pushState={pushState} className="preview" page={page} pages={pages} totalCount={totalCount} search={search} tags={tags} tag={tag}/>
