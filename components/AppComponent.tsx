bc06b818799cf90d57a0c83c1f695147f87725e8
Shmulik Flint <shmulikf@wix.com>
2021-08-19 23:29:04 +0300

    Improve "show more" style

--- components/AppComponent.tsx
+++ components/AppComponent.tsx
@@ -242,7 +242,7 @@ export function AppComponent(appProps: AppProps & { authData: AuthData }): React
                     </div>
                     <input type="checkbox" id={page.title} defaultChecked={pages.length === 1}/>
                     <PageHtmlRenderer pushState={pushState} className="preview" page={page} pages={pages} totalCount={totalCount} search={search} tags={tags} tag={tag}/>
-                    <label htmlFor={page.title} role="button">הצג עוד</label>
+                    <label htmlFor={page.title} role="button">הצג עוד...</label>
                   </div>
                 </div>)
               }
