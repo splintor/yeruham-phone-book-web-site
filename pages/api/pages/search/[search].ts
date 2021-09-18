aa309f94b610b5aed6032191bcdae2902643ec05
Shmulik Flint <shmulikf@wix.com>
2021-09-18 22:11:34 +0300

    Remove unused import

--- pages/api/pages/search/[search].ts
+++ pages/api/pages/search/[search].ts
@@ -1,5 +1,5 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { getRequestLogData, sendResponse } from '../../../../utils/api'
+import { sendResponse } from '../../../../utils/api'
 import { searchPages } from '../../../../utils/data-layer'
 
 export default async function search(request: NextApiRequest, response: NextApiResponse): Promise<void> {
