25a2bd1c477cf8e1217b2ddd81eb9cb78da28758
Shmulik Flint <shmulikf@wix.com>
2021-10-03 02:05:11 +0300

    Update home page description

--- components/App.tsx
+++ components/App.tsx
@@ -43,7 +43,7 @@ export default function App(appProps: AppProps): ReactElement {
         ? page?.tags?.includes(publicTagName)
           ? page.html.replace(/<[^>]+>|&nbsp;/g, ' ')
           : ''
-        : 'כל הפרטים על מוסדות, עסקים ואנשים בירוחם. פרטי המוסדות הציבוריים פתוחים לכולם. פרטי התושבים נגישים לתושבי ירוחם בלבד. תושבי ירוחם גם יכולים לערוך את הפרטים באתר ולדאוג שהוא ישאר מעודכן'
+        : 'כל הפרטים על מוסדות, עסקים ואנשים בירוחם. פרטי המוסדות והעסקים פתוחים לכולם. פרטי התושבים נגישים לתושבי ירוחם בלבד. תושבי ירוחם גם יכולים לערוך את הפרטים באתר ולדאוג שהוא ישאר מעודכן.'
 
   const [authData, setAuthData] = useState<AuthData>()
   useEffect(() => setAuthData(parseAuthCookies()), [])
