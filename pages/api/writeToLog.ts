ee08a2d86f015316e4ddf052cf19449f13e7c9d9
Shmulik Flint <shmulikf@wix.com>
2021-12-26 08:15:51 +0200

    test2

--- pages/api/writeToLog.ts
+++ pages/api/writeToLog.ts
@@ -1,10 +1,11 @@
 import { NextApiRequest, NextApiResponse } from 'next'
+import { parseAuthCookies } from '../../utils/cookies'
 // import { sendResponse, sendUnsupportedMethodResponse } from '../../utils/api'
 // import { sendToLog } from '../../utils/data-layer'
 
 export default async function writeToLog(request: NextApiRequest, response: NextApiResponse): Promise<void> {
-  response.setHeader("Content-Type", "text/xml")
-  response.write(request.method)
+  response.setHeader('Content-Type', 'application/json; charset=utf-8')
+  response.write(parseAuthCookies(request).auth)
   response.end()
   // switch (request.method) {
   //   case 'POST':
