4ea72c34d8008a06f235f6fcd6430ea5231d6503
Shmulik Flint <shmulikf@wix.com>
2021-12-26 08:12:41 +0200

    test

--- pages/api/writeToLog.ts
+++ pages/api/writeToLog.ts
@@ -1,13 +1,16 @@
 import { NextApiRequest, NextApiResponse } from 'next'
-import { sendResponse, sendUnsupportedMethodResponse } from '../../utils/api'
-import { sendToLog } from '../../utils/data-layer'
+// import { sendResponse, sendUnsupportedMethodResponse } from '../../utils/api'
+// import { sendToLog } from '../../utils/data-layer'
 
 export default async function writeToLog(request: NextApiRequest, response: NextApiResponse): Promise<void> {
-  switch (request.method) {
-    case 'POST':
-      return sendResponse(response, await sendToLog(request))
-
-    default:
-      sendUnsupportedMethodResponse(response)
-  }
+  response.setHeader("Content-Type", "text/xml")
+  response.write(request.method)
+  response.end()
+  // switch (request.method) {
+  //   case 'POST':
+  //     return sendResponse(response, await sendToLog(request))
+  //
+  //   default:
+  //     sendUnsupportedMethodResponse(response)
+  // }
 }
