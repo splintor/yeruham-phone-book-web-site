6b6c9563c52c178633b758efe89175a4eaaf4e42
Shmulik Flint <shmulikf@wix.com>
2021-12-26 08:05:33 +0200

    rename /log to /writeToLog

--- pages/api/writeToLog.ts
+++ pages/api/writeToLog.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next'
 import { sendResponse, sendUnsupportedMethodResponse } from '../../utils/api'
 import { sendToLog } from '../../utils/data-layer'
 
-export default async function log(request: NextApiRequest, response: NextApiResponse): Promise<void> {
+export default async function writeToLog(request: NextApiRequest, response: NextApiResponse): Promise<void> {
   switch (request.method) {
     case 'POST':
       return sendResponse(response, await sendToLog(request))
