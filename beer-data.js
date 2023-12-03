ae4b03a6da553c4548248ebd17b7b44b24b1688f
Shmulik Flint <shmulikf@wix.com>
2023-12-03 07:48:52 +0200

    data extraction
    Not sure why it is needed...

---
+++ beer-data.js
@@ -0,0 +1,21 @@
+const { pages } = require('./beer.json');
+const csv = [];
+
+for (let p of pages) {
+  if (p.isDeleted) {
+    continue;
+  }
+  if (!p.html) {
+    console.log(p);
+  } else {
+    const phoneNumbers = Array.from(p.html?.matchAll(/\D((\d-?){9,10})\D/g)).map(r => r[1].replace(/-/g, ''))
+    if (phoneNumbers.length > 0) {
+      csv.push([p.title, ...phoneNumbers, ...p.tags]);
+    // } else {
+    //   console.log('No numbers: ', p.title);
+    }
+  }
+}
+
+// console.log('CSV count: ', csv.length);
+console.log(csv.join('\n'));
